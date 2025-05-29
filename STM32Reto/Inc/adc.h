4b5ed9cbc7e842885d2e5ad6f13f72472532f675
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-05-29 01:08:45 -0600

    Update adc.h

--- STM32Reto/Inc/adc.h
+++ STM32Reto/Inc/adc.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-void USER_ADC_Init(void);
-uint16_t USER_ADC_Read(void);
-void USER_ADC_Calibration(void);
+uint8_t USER_ADC_Calibration( void );
+void USER_ADC_Init( void );
+uint16_t USER_ADC_Read( void );
 
 #endif /* USER_ADC_H_ */
