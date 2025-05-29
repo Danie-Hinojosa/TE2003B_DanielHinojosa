2ea5bb7cfc15a0a9f961c89b65eae8bd2f291cea
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-05-29 01:10:12 -0600

    Update user_uart.h

--- STM32Reto/Inc/user_uart.h
+++ STM32Reto/Inc/user_uart.h
@@ -4,11 +4,10 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define USART1_BASE 0x40013800UL
-
-void USER_USART1_Init(void);
+void USER_USART1_Init( void );
+void USER_USART1_Transmit( uint8_t *pData, uint16_t size );
+void USER_USART1_Send_8bit(uint8_t Data);
 uint8_t USER_USART1_Receive_8bit(void);
-void USER_USART1_Transmit(uint8_t *pData, uint16_t size);
-int _write(int file, char *ptr, int len);
+void USART1_IRQHandler(void);
 
 #endif /* USER_UART_H_ */
