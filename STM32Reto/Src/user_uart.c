1577f314434773a09064777d6cf1080df713ebe0
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-05-29 01:13:49 -0600

    Update user_uart.c

--- STM32Reto/Src/user_uart.c
+++ STM32Reto/Src/user_uart.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include "user_uart.h"
 
+#define RX_BUFFER_SIZE 64
+
 static void USART1_SendByte(uint8_t byte);
+char rx_buffer[RX_BUFFER_SIZE];
+uint8_t rx_index = 0;
 
 void USER_USART1_Init(void) {
     // Activar reloj de GPIOA y USART1
@@ -24,29 +28,67 @@ void USER_USART1_Init(void) {
     USART1->BRR  = 417;  // Para 115200 baudios @ 48 MHz
 
     // Habilitar USART, transmisión y recepción
-    USART1->CR1 |= (1 << 0) | (1 << 2) | (1 << 3);
+    USART1->CR1 |= (1 << 0) | (1 << 2) | (1 << 3) | (1 << 5);
+
+    NVIC_ISER0 = (1UL << 27);
 }
 
-static void USART1_SendByte(uint8_t byte) {
-    while (!(USART1->ISR & (1 << 7)));  // Espera a que el TDR esté libre
-    USART1->TDR = byte;
+void USER_USART1_Send_8bit(uint8_t Data) {
+	while (!( USART1->ISR & (0x1UL << 7U)))
+		; // wait until next data can be written
+	USART1->TDR = Data; // Data to send
 }
 
 void USER_USART1_Transmit(uint8_t *pData, uint16_t size) {
-    for (uint16_t i = 0; i < size; ++i) {
-        USART1_SendByte(pData[i]);
-    }
+	for (int i = 0; i < size; i++) {
+		USER_USART1_Send_8bit(*pData++);
+	}
 }
 
 uint8_t USER_USART1_Receive_8bit(void) {
-    while (!(USART1->ISR & (1 << 5)));  // Espera dato en RDR
-    return (uint8_t)(USART1->RDR);
+	while (!( USART1->ISR & (1UL << 5U)))
+		; // Espera hasta que RXNE esté en 1 (dato recibido)
+	return (uint8_t) (USART1->RDR & 0xFF); // Lee el dato recibido
+}
+
+void USART1_IRQHandler(void) {
+    if (USART1->ISR & (1UL << 5U)) { // RXNE
+        uint8_t received = (uint8_t)(USART1->RDR & 0xFF);
+
+        if(received == 'I') {
+            rx_index = 0;
+            memset(rx_buffer, 0, RX_BUFFER_SIZE);
+            rx_buffer[rx_index++] = received;
+            return;
+        }
+
+        if (rx_buffer[0] == 'I') {
+            if(received == 'E') {
+                rx_buffer[rx_index] = '\0';
+
+                int rpm_i1, rpm_i2, vl_i1, vl_i2, gear_i;
+                if (sscanf(&rx_buffer[1], "%d,%d,%d,%d,%d,", &rpm_i1, &rpm_i2, &vl_i1, &vl_i2, &gear_i) == 5) {
+                    rpm = rpm_i1 + (rpm_i2 / 100.0f);
+                    vl = vl_i1 + (vl_i2 / 100.0f);
+                    gear = (float)gear_i;
+                    paqueteListo = 1;
+                }
+            } else if (rx_index < RX_BUFFER_SIZE - 1) {
+                rx_buffer[rx_index++] = received;
+            } else {
+                rx_index = 0;
+                memset(rx_buffer, 0, RX_BUFFER_SIZE);
+            }
+        }
+    }
 }
 
 int _write(int file, char *ptr, int len) {
-    for (int i = 0; i < len; ++i) {
-        while (!(USART1->ISR & (1 << 7)));
-        USART1->TDR = ptr[i];
-    }
-    return len;
+	int DataIdx;
+	for (DataIdx = 0; DataIdx < len; DataIdx++) {
+		while (!( USART1->ISR & (0x1UL << 7U)))
+			;
+		USART1->TDR = *ptr++;
+	}
+	return len;
 }
