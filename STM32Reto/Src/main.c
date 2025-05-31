9ffe2008936714bd1759af20db74e2bfa1a24732
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-05-30 22:12:44 -0600

    Update main.c

--- STM32Reto/Src/main.c
+++ STM32Reto/Src/main.c
@@ -14,7 +14,7 @@ void transmit_data(void);
 void Update_Inputs(void);
 void USER_Delay(void);
 
-volatile uint8_t paqueteListo = 0;
+volatile uint8_t paqueteListo = 1;
 volatile uint32_t tim16_tick = 0;
 
 int vl = 0;
@@ -25,6 +25,10 @@ int button_state = 0;
 
 int main(void)
 {
+	//Local variables
+	uint32_t start_tx = 0;
+	uint32_t start_led = 0;
+
 	/* Initialization of Peripherals */
 	USER_RCC_Init(); 				// Set CLK to 48MHz
 	USER_SysTick_Init();		//
@@ -36,47 +40,32 @@ int main(void)
 	USER_ADC_Init();
 	USER_TIM16_Init();
 
-	//Local variables
-	uint32_t start_tx = 0;
-	uint32_t start_led = 0;
-
 	char *msg = "Prueba de UART desde STM32\n";
 	USER_USART1_Transmit((uint8_t*)msg, strlen(msg));
 
-  for(;;){
-		if (paqueteListo) {
-			paqueteListo = 0;
-			Update_PWM_From_Velocity(vl);
-			// Aquí puedes procesar los datos almacenados en rpm, vl y gear
-      Update_PWM_From_Velocity(vl);
-		}
+	for (;;) {
+			Update_PWM_From_Velocity(vl);  // Ajustar LED PWM basado en velocidad
 
-		if (delay_elapsed(&start_tx, 100)) {
-			Update_Inputs();
 			LCD_Clear();
-			LCD_Set_Cursor(1,1);
-			LCD_Put_Str("Rpm: ");
-			LCD_Put_Num(rpm);
+			LCD_Set_Cursor(1, 1);
+			LCD_Put_Str("Spd:");
+			LCD_Put_Num(vl);
 
-			LCD_Set_Cursor(2,1);
-			LCD_Put_Str("Gr:");
+			LCD_Set_Cursor(1, 9);
+			LCD_Put_Str("G:");
 			LCD_Put_Num(gear);
 
-			LCD_Set_Cursor(2,8);
-			LCD_Put_Str("Sp: ");
-			LCD_Put_Num(vl);
-			transmit_data();
+			LCD_Set_Cursor(2, 1);
+			LCD_Put_Str("RPM:");
+			LCD_Put_Num(rpm);  // Mostrar velocidad
+
+			delay_ms(10);
+
+		if (delay_elapsed(&start_tx, 100)) {
+			Update_Inputs();  // Solo se usa para transmisión de ADC y botón
+			transmit_data();  // Envía ADC y botón al ESP32
 		}
-
-		if (delay_elapsed(&start_led, 100)) {
-			GPIOA->ODR ^= (0x1UL << 5U); // Toggle USER LED
-
-
-		}
-
-
 	}
-
 }
 
 void USER_RCC_Init(void) {
@@ -95,10 +84,10 @@ void USER_RCC_Init(void) {
 void USER_GPIO_Init(void) {
 	RCC->IOPENR = RCC->IOPENR | (0x1UL << 0U);
 
-	// Configure PA6 as input w pull up
-	GPIOA->PUPDR = GPIOA->PUPDR & ~(0x2UL << 12U);
-	GPIOA->PUPDR = GPIOA->PUPDR | (0x1UL << 12U);
-	GPIOA->MODER = GPIOA->MODER & ~(0x3UL << 12U);
+	// Configure PA3 as input w pull up
+	GPIOA->PUPDR = GPIOA->PUPDR & ~(0x3UL << 6U);
+	GPIOA->PUPDR = GPIOA->PUPDR | (0x1UL << 6U);
+	GPIOA->MODER = GPIOA->MODER & ~(0x3UL << 6U);
 
 	// Configure PA5 as output push pull (It comes in the LCD, so JIC)
 	GPIOA->BSRR   = 0x1UL << 21U; // Reset PA5 low to turn off LED
@@ -108,44 +97,14 @@ void USER_GPIO_Init(void) {
 	GPIOA->MODER  = GPIOA->MODER  |  (0x1UL << 10U); // Set PA5 as output
 }
 
-void USER_Delay(void){
-  __asm(" ldr r0, =12999");
-  __asm(" again: sub r0, r0, #1");
-  __asm(" cmp r0, #0");
-  __asm(" bne again");
-  __asm(" nop");
-  __asm(" nop");
-  __asm(" nop");
-  __asm(" nop");
-  __asm(" nop");
-  __asm(" nop");
-}
-
 void transmit_data() {
-    uint8_t tx_buffer[64];
+	uint8_t tx_buffer[32];
 
-    int rpm_i = (int)(rpm * 100);
-    int vl_i = (int)(vl * 100);
-    int gear_i = (int)(gear);
-    int acc_i1 = acceleration / 100;
-    int acc_i2 = acceleration % 100;
-    int btn_i = (int)(button_state);
-
-    sprintf((char*)tx_buffer,
-            "I%d.%02d,%d.%02d,%d,%d.%02d,%d,E\n",
-            rpm_i / 100, rpm_i % 100,
-            vl_i / 100,  vl_i % 100,
-            gear_i,
-            acc_i1,
-            acc_i2,
-            btn_i);
-
-    printf("STM32 ENVIA: %s", tx_buffer);  // semihosting o redirección por UART
-
+	sprintf((char*)tx_buffer, "ADC:%d BTN:%d\n", acceleration, button_state);
     USER_USART1_Transmit(tx_buffer, strlen((char*)tx_buffer));
 }
 
 void Update_Inputs(void) {
     acceleration = USER_ADC_Read();
-    button_state = (GPIOA->IDR & (1UL << 6U)) ? 0 : 1;
+    button_state = (GPIOA->IDR & (1UL << 3U)) ? 1 : 0;
 }
