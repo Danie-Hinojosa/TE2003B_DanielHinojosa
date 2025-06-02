2d517e324255687964b98a77358c4012445d5f34
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-06-02 11:58:32 -0600

    Update user_core.c

--- STM32Reto/Src/user_core.c
+++ STM32Reto/Src/user_core.c
@@ -3,24 +3,6 @@
 #include "user_core.h"
 #include "user_tim.h"
 
-void USER_SysTick_Init( void ){
-  // Reloj del sistema = 48 MHz
-  // SysTick es de 24 bits, así que máximo 2^24-1 = 16,777,215
-  // Para 1ms: 48,000,000 / 1000 ciclos
-  Systick->RVR  = 48000 - 1;//          Carga para 1ms
-  Systick->CVR  = 0;//                  Reinicia el contador
-  Systick->CSR |=  ( 0x1UL <<  2U );//  Selecciona el reloj del procesador como el Systick
-}
-
-void SysTick_Delay( uint32_t time ) {
-  Systick->CSR |=  ( 0x1UL <<  0U );//  Inicia el Systick
-  for(uint32_t i = 0; i < time; i++){
-    // Espera a que el bit COUNTFLAG se ponga en 1 (cuando se desborda)
-    while(!( Systick->CSR & ( 0x1UL << 16U )));
-  }
-  Systick->CSR &= ~( 0x1UL <<  0U );//  Detiene el SysTick
-}
-
 void delay_ms(uint32_t ms) {
     for (uint32_t i = 0; i < ms; i++) {
         for (volatile uint32_t j = 0; j < 48000; j++) {
@@ -45,3 +27,4 @@ uint8_t delay_elapsed(uint32_t *start, uint32_t n_ticks) {
     }
     return 0;
 }
+
