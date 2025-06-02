91c0d1b699ad034796b78faef3b86bcfb4d04666
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-06-02 12:01:15 -0600

    Update user_core.h

--- STM32Reto/Inc/user_core.h
+++ STM32Reto/Inc/user_core.h
@@ -5,7 +5,7 @@
 
 uint8_t delay_elapsed(uint32_t *start, uint32_t n_ticks);
 
-void delay_ms_TIM16(uint32_t ms);
-void delay_us_TIM16(uint32_t us);
+void delay_ms(uint32_t ms);
+void delay_us(uint32_t us);
 
 #endif /* USER_CORE_H_ */
