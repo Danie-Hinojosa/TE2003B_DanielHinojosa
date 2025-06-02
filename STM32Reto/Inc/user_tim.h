d38b35a7edfef12e356a6e59d7d01cf46d5e31ae
Danie-Hinojosa <110929165+Danie-Hinojosa@users.noreply.github.com>
2025-06-02 12:01:40 -0600

    Update user_tim.h

--- STM32Reto/Inc/user_tim.h
+++ STM32Reto/Inc/user_tim.h
@@ -7,9 +7,11 @@ void USER_TIM3_PWM_Init(void);
 uint16_t USER_Duty_Cycle(uint8_t duty);
 void USER_Set_PWM_Duty(uint8_t duty_percent);
 void USER_TIM14_Init(void);
-void USER_TIM14_Delay(uint16_t prescaler, uint16_t AutoReload);
+void USER_TIM14_Delay(uint16_t ms);
 void USER_TIM16_Init(void);
+void USER_TIM17_Init_Timer(void);
 void TIM16_IRQHandler(void);
-void Update_PWM_From_Velocity(float);
+void Update_PWM_From_Velocity(int velocity_percent);
 
 #endif /* USER_TIM_H_ */
+
